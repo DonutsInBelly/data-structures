f8031724d466e689949893aaaa7c8c2e45eaea1d
Carlin Au <carlinau@gmail.com>
2017-06-13 12:25:27 -0400

    Added toString method for TreeNode

--- structures/TreeNode.java
+++ structures/TreeNode.java
@@ -10,18 +10,7 @@ public class TreeNode {
     this.children = new ArrayList<TreeNode>();
   }
 
-  public void print() {
-        print("", true);
-  }
-
-  private void print(String prefix, boolean isTail) {
-    System.out.println(prefix + (isTail ? "└── " : "├── ") + value);
-    for (int i = 0; i < children.size() - 1; i++) {
-      children.get(i).print(prefix + (isTail ? "    " : "│   "), false);
-    }
-    if (children.size() > 0) {
-      children.get(children.size() - 1)
-              .print(prefix + (isTail ?"    " : "│   "), true);
-    }
+  public String toString() {
+    return "[ " + value + " ]-->";
   }
 }
